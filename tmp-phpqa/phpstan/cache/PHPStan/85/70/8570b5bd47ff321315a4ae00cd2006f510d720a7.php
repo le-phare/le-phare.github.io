dd97047eb2eb1141700c3c2a04c3a5994d6e6ff6
theophile.jerome-rocher@lephare.com <theophile.jerome-rocher@lephare.com>
2023-10-30 11:03:43 +0100

    fix: tmp-phqa remove

--- tmp-phpqa/phpstan/cache/PHPStan/85/70/8570b5bd47ff321315a4ae00cd2006f510d720a7.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types = 1);
-
-// variadic-function-getDataFromVersionfile-/project/tools/trigger_generator_versions_docs.php
-return \PHPStan\Cache\CacheItem::__set_state(array(
-   'variableKey' => '1698659346-v3',
-   'data' => false,
-));
