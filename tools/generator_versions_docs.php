198cdb8722d2b310e6492c7315a683d0d4f52588
theophile.jerome-rocher@lephare.com <theophile.jerome-rocher@lephare.com>
2023-11-14 12:57:13 +0100

    feat: new template system
    we exec template.md.php with the json path file, we redirect output to new .md file

--- tools/generator_versions_docs.php
+++ tools/generator_versions_docs.php
@@ -3,44 +3,7 @@
 const FOLDER_VERSIONS_PATH = '../versions_data/';
 const VERSIONS_PAGES_SITE_FOLDER = '../docs/generated/versions_pages/';
 const VERSIONS_SCRIPTS_FOLDER = '../docs/generated/versions_tests_scripts/';
-
-function cleanOperatorValue(string $value): string
-{
-    if ('<' == $value[0] or '>' == $value[0]) {
-        if ('=' == $value[1]) {
-            $value = substr($value, 2);
-        } else {
-            $value = substr($value, 1);
-        }
-    }
-
-    return $value;
-}
-
-/**
- * @param array<mixed, mixed> $array
- */
-function arrayToMarkdownList(array $array): string // arrays are automatically markdown list str in template.
-{
-    $markdownList = '';
-
-    foreach ($array as $key => $value) {
-        if (is_string($key)) { // mostly php.ini
-            if ('_' != substr($key, 0, 1)) { // not a array comment _comment1 like.
-                $markdownList .= "\t".$key.' = '.cleanOperatorValue($value)."\n";
-            } else {
-                $markdownList .= "\t".$value."\n";
-            }
-        } else {
-            if ('_' === substr($value, 0, 1)) {
-                $value = substr($value, 1);
-            }
-            $markdownList .= '* '.$value."\n"; // normal list
-        }
-    }
-
-    return $markdownList;
-}
+const TEMPLATES_FOLDER = './templates/';
 
 /**
  * @param array<mixed, mixed> $json
@@ -52,10 +15,6 @@ function templateManage(string $templateContent, array $json): string
 
     foreach ($json as $key => $value) {
         $search[] = '{{'.$key.'}}';
-        if (is_array($value)) {
-            $fill[] = arrayToMarkdownList($value);
-            continue;
-        }
         $fill[] = $value;
     }
 
@@ -81,25 +40,23 @@ function getDataFromVersionfile(string $filePath, bool $directPath): array
 /**
  * @param array<mixed, mixed> $versionJson
  */
-function handleVersionfileJson(array $versionJson): void
+function handleVersionfileJson(array $versionJson, string $entry): void
 {
     $fullJson = array_merge_recursive(getDataFromVersionfile('shared.json', false), $versionJson);
 
-    generateNewVersionsFiles($fullJson);
+    generateNewVersionsFiles($fullJson, $entry);
 }
 
 /**
  * @param array<mixed, mixed> $json
  */
-function generateMarkdownFile(array $json, string $newfilePath): void
+function generateMarkdownFile(array $json, string $newfilePath, string $entry): void
 {
     echo ' * Generating markdown file : '.$newfilePath."\n";
-    $template = file_get_contents('./templates/template.md');
-    if (false === $template) {
-        return;
+    $status = exec('php '.TEMPLATES_FOLDER.'template.md.php "'.FOLDER_VERSIONS_PATH.$entry.'" > '.$newfilePath, $output, $return);
+    if ($return) {
+        echo "\033[0;31mGenerating markdown file via exec() failed\033[0m\n";
     }
-    $filledContent = templateManage($template, $json);
-    file_put_contents($newfilePath, $filledContent);
 }
 
 /**
@@ -108,7 +65,7 @@ function generateMarkdownFile(array $json, string $newfilePath): void
 function generatePhpcheckFile(array $json, string $newfilePath): void
 {
     echo ' * Generating php check script : '.$newfilePath."\n";
-    $template = file_get_contents('./templates/check_version_script_template.php');
+    $template = file_get_contents(TEMPLATES_FOLDER.'check_version_script_template.php');
     if (false === $template) {
         return;
     }
@@ -119,13 +76,13 @@ function generatePhpcheckFile(array $json, string $newfilePath): void
 /**
  * @param array<mixed, mixed> $fullJson
  */
-function generateNewVersionsFiles(mixed $fullJson): void // .md & php
+function generateNewVersionsFiles(mixed $fullJson, string $entry): void // .md & php
 {
     $phpscriptFilepath = VERSIONS_SCRIPTS_FOLDER.'check_'.$fullJson['version'].'.php';
     $markdownFilepath = VERSIONS_PAGES_SITE_FOLDER.$fullJson['version'].'.md';
 
     echo "\033[92mFAROS VERSION ".$fullJson['version']." --> Generating files....\033[0m\n";
-    generateMarkdownFile($fullJson, $markdownFilepath);
+    generateMarkdownFile($fullJson, $markdownFilepath, $entry);
     generatePhpcheckFile($fullJson, $phpscriptFilepath);
     echo "\n";
 }
@@ -141,7 +98,7 @@ function generateAllVersions(): void
                     continue;
                 }
                 $json = getDataFromVersionfile($entry, false);
-                handleVersionfileJson($json);
+                handleVersionfileJson($json, $entry);
             }
         }
         closedir($folder);
@@ -151,8 +108,7 @@ function generateAllVersions(): void
 function generateOneVersion(string $filePath): void
 {
     $json = getDataFromVersionfile($filePath, true);
-
-    handleVersionfileJson($json);
+    handleVersionfileJson($json, basename($filePath));
 }
 
 function main(): void
---
+++ tools/templates/template.md.php
@@ -0,0 +1,169 @@
+<?php
+    if (2 != $argc) {
+        exit("Usage: php template.md.php chemin_vers_json\n");
+    }
+    $jsonPath = $argv[1];
+    $versionDataAlone = json_decode((string) file_get_contents($jsonPath), true);
+    $sharedJson = json_decode((string) file_get_contents('../versions_data/shared.json'), true);
+    if (!$sharedJson || !$versionDataAlone) {
+        exit("Failed reading or decoding a json file (version/shared) in template.md.php\n");
+    }
+    $versionData = (object) array_merge_recursive($versionDataAlone, $sharedJson);
+?>
+---
+layout: default
+title: <?php echo $versionData->fullVersionName; ?>
+
+nav_order: <?php echo $versionData->order_in_list; ?>
+
+parent: Versions
+permalink: docs/versions/<?php echo $versionData->version; ?>.html
+
+--- 
+<div class="callout callout-info" markdown="span">
+Pour être sûr que la machine est bien configurée --> tout doit être en vert sur le script PHP, toutes les checkbox sur ce document cochées.
+</div>
+
+# <?php echo $versionData->version; ?>
+
+
+1. TOC
+{:toc}
+
+## Check
+
+<input type="checkbox"/> Le script [check_<?php echo $versionData->version; ?>.php](../versions_tests_scripts/check_<?php echo $versionData->version; ?>.php) est à disposition pour check une bonne partie des prérequis.
+Tous les voyants devraient être verts, attention cependant il est possible d'avoir de faux négatifs (par exemple si la mémoire configurée est supérieure à celle requise).
+
+## La stack de base
+- <input type="checkbox"/> Debian <?php echo $versionData->debian_version; ?>
+
+- <input type="checkbox"/> PHP <?php echo $versionData->php_version; ?>
+
+- <input type="checkbox"/> Apache <?php echo $versionData->apache_version; ?>
+
+- <input type="checkbox"/> PostgreSQL <?php echo $versionData->pgsql_version; ?>
+
+
+## Dimensionnement machine
+
+Nous exigeons au minimum :
+ * <input type="checkbox"/> <?php echo $versionData->expected_vcpus; ?> vCPU
+ * <input type="checkbox"/> <?php echo $versionData->expect_ram_go; ?> Go RAM
+
+## Authentification SSH
+
+<input type="checkbox"/> La liste des clés publiques des utilisateurs du Phare est disponible à l'adresse suivante : [https://faros.lephare.com/lephare.keys].
+
+	curl -sL https://faros.lephare.com/lephare.keys >> ~/.ssh/authorized_keys
+	chmod 0600 ~/.ssh/authorized_keys
+
+<input type="checkbox"/> La liste des adresses ips à autoriser : 
+<?php
+  foreach ($versionData->ip_to_authorize as $ip) {
+      echo '* '.$ip." \n";
+  }
+?>
+
+
+## Binaires
+<?php
+  foreach ($versionData->binaries_to_display as $bin) {
+      echo '* <input type="checkbox"/> '.$bin." \n";
+  }
+?>
+
+
+## Droits
+
+### Logs
+
+Créer un user séparé qui a uniquement accès en lecture aux fichiers de logs suivants :
+
+- <input type="checkbox"/> Apache access.log
+- <input type="checkbox"/> Apache error.log
+- <input type="checkbox"/> PHP-FPM log
+
+## Configuration PHP
+
+### Extensions
+
+Pré-requis pour Symfony 6.x
+
+<?php
+  foreach ($versionData->symfony_requirements as $requirement) {
+      if ('_' == $requirement[0]) {
+          $requirement = substr($requirement, 1);
+      }
+      echo '* '.$requirement." \n";
+  }
+?>
+
+Extensions supplémentaires pour nos applications
+<?php
+  foreach ($versionData->faros_requirements as $requirement) {
+      if ('_' == $requirement[0]) {
+          $requirement = substr($requirement, 1);
+      }
+      echo '* '.$requirement." \n";
+  }
+?>
+
+### php.ini
+<?php
+  function cleanOperatorValue(string $value): string
+  {
+      if ('<' == $value[0] or '>' == $value[0]) {
+          if ('=' == $value[1]) {
+              $value = substr($value, 2);
+          } else {
+              $value = substr($value, 1);
+          }
+      }
+
+      return $value;
+  }
+  foreach ($versionData->settings as $key => $value) {
+      if ('_' != substr($key, 0, 1)) {
+          echo "\t".$key.' = '.cleanOperatorValue($value)."\n";
+      } else {
+          echo "\t".$value."\n";
+      }
+  }
+?>
+
+## Configuration Apache
+
+### <input type="checkbox"/> SSL & HTTP/2
+
+Chaque vhost doit être accessible en HTTPS et HTTP/2.
+
+### <input type="checkbox"/> Configuration vhost
+
+	DocumentRoot <deploy_dir>/current/public/
+	Options FollowSymLinks
+	Protocols h2 http/1.1
+
+### Modules
+
+- <input type="checkbox"/> mod_rewrite On
+- <input type="checkbox"/> mod_headers On
+- <input type="checkbox"/> mod_expires On
+- <input type="checkbox"/> mod_deflate On
+
+
+<style>
+.callout.callout-info {
+  background-color: #3498db; /* Couleur de fond bleue */
+  color: #fff; /* Couleur du texte en blanc */
+  border-left: 5px solid #2980b9; /* Bordure gauche bleue plus foncée */
+  padding: 10px; /* Espace intérieur */
+  margin: 10px 0; /* Marge extérieure */
+  border-radius: 5px; /* Coins arrondis */
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.3); /* Ombre légère */
+}
+
+.callout.callout-info p {
+  margin: 0; /* Supprime la marge du texte à l'intérieur du callout */
+}
+</style>
